57e01f16addf87678a1a29d00b15cbd8fdf315c2
mazhenguo666 <275331264@qq.com>
2018-09-19 20:36:24 +0800

    ''

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONF = {
     // 其他配置 ...
-    serverHost: '127.0.0.1',
+    serverHost: '59.110.166.225',
     tunnelServerUrl: '',
     tunnelSignatureKey: '27fb7d1c161b7ca52d73cce0f1d833f9f5b5ec89',
       // 腾讯云相关配置可以查看云 API 秘钥控制台：https://console.cloud.tencent.com/capi
